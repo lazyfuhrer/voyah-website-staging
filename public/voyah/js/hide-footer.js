8451078a7c21621adb26ae0dd15d6c9c3b3bf2ae
Biswarghya Biswas <iambiswarghya@gmail.com>
2026-04-04 14:42:59 +0530

    fix

--- public/voyah/js/hide-footer.js
+++ public/voyah/js/hide-footer.js
@@ -1,16 +1,5 @@
 window.addEventListener("load", function () {
 
-  function removeContact() {
-    document.querySelectorAll('nav a').forEach(el => {
-      const href = el.getAttribute("href") || "";
-
-      if (href.includes("contact")) {
-        const li = el.closest("li");
-        if (li) li.remove();
-      }
-    });
-  }
-
   function removeFooterContactInfo() {
     const el = document.querySelector(
       "#colophon div.w-full.md\\:w-2\\/5:not(.max-md\\:mt-5)"
@@ -26,13 +15,11 @@ window.addEventListener("load", function () {
   }
 
   // Run once
-  removeContact();
   removeFooterContactInfo();
   removeFooterPolicies();
 
   // Watch for Alpine rendering / changes
   const observer = new MutationObserver(() => {
-    removeContact();
     removeFooterContactInfo();
     removeFooterPolicies();
   });
